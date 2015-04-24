d58652a8045fae6b77b6c763c555b966cd74eaec
Temirkhan <metallhan@gmai.com>
2015-04-24 19:09:40 +0600

    task

---
+++ task1.js
@@ -0,0 +1,7 @@
+var i = []
+var s=0
+	for(var i=0;i<10;i++) {
+ s=s+i
+        console.log(i)
+    }
+console.log('summa=',s)
---
+++ task2.js
@@ -0,0 +1,8 @@
+var i = []
+var s=0
+	for(var i=1;i<=10;i++) {
+        
+  s=s+i
+     console.log(i,[i-(i+1)])
+    }
+  
