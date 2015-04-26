48e455423087a24dbb5d0816a299799e8658ebe9
Temirkhan <metallhan@gmai.com>
2015-04-26 18:05:20 +0600

    1(peredel)

--- task1.js
+++ task1.js
@@ -1,9 +1,9 @@
-var msv = [3,6,9,5,12,15];
+var m = [3,6,9,5,12];
+console.log(m);
 var i;
-var s=0
-	for(i=0;i<6;i++)
+var s=0;
+	for(i=0;i< m.length;i++)
      {
- s=s+msv[i];
+ s=s+m[i];
      }
-console.log(msv);
 console.log('summa=',s);
